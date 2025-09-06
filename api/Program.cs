659645d1be7cc85f777a7c098d6c6e6640dd4846
kanompang962 <kanompang962@gmail.com>
2025-09-06 23:33:57 +0700

    chore(api): config Dockerfile

--- api/Program.cs
+++ api/Program.cs
@@ -6,6 +6,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
@@ -15,6 +26,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Simple endpoints
+app.MapGet("/", () => new { message = "Hello from .NET 8 API!", timestamp = DateTime.Now });
+app.MapGet("/health", () => new { status = "Healthy", timestamp = DateTime.Now });
+
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
