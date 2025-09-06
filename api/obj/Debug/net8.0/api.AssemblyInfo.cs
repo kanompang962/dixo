c96884fd6b134d75468fe14dddad73e14b7a1d8a
kanompang962 <kanompang962@gmail.com>
2025-09-06 21:06:32 +0700

    chore(web): config Dockerfile

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919b8d6f9947704991fce3d694f75847b0ebe5c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d421ec9ec6333d3c02a6407bdea2232120acf5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
