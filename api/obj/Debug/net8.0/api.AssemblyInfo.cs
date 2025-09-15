cae9bb462dddef9510c8271a9f3a0aa51b3eaa83
kanompang962 <kanompang962@gmail.com>
2025-09-15 11:46:26 +0700

    ci(cicd): ตรวจสอบว่าไฟล์ไหนถูกแก้ไข ถึงจะup

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79058d11249e60f1dc6db073c909063522c8e7ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+054327b365e121a2af0396a019d7f739b3f5fd5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
