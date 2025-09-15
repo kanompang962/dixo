cc1327dcac7825882ea99694a08eb2b953b61970
kanompang962 <kanompang962@gmail.com>
2025-09-16 00:56:57 +0700

    test(web): mock component test routerlink

--- web/src/app/core/layouts/admin-layout/admin-layout.spec.ts
+++ web/src/app/core/layouts/admin-layout/admin-layout.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AdminLayout } from './admin-layout';
+import { provideRouter } from '@angular/router';
+import { Component } from '@angular/core';
+
+// Dummy component สำหรับ router-outlet
+@Component({
+  standalone: true,
+  template: `<p>Dummy Works!</p>`
+})
+class DummyComponent {}
 
 describe('AdminLayout', () => {
   let component: AdminLayout;
@@ -8,9 +16,15 @@ describe('AdminLayout', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AdminLayout]
-    })
-    .compileComponents();
+      imports: [AdminLayout], // standalone component
+      providers: [
+        provideRouter([
+          { path: '', component: DummyComponent },
+          { path: 'user-management', component: DummyComponent },
+          { path: 'items', component: DummyComponent },
+        ])
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AdminLayout);
     component = fixture.componentInstance;
@@ -19,5 +33,12 @@ describe('AdminLayout', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render drawer items', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.textContent).toContain('Dashboard');
+    expect(compiled.textContent).toContain('User Management');
+    expect(compiled.textContent).toContain('Items');
   });
 });
